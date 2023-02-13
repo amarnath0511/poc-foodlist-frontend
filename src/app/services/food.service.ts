7e298a5460c5e1cfb698b05590f96d7e8a6b9b3e
Amarnath Natarajan <amarnath.n@domain.rage-india.com>
2023-02-13 17:29:11 +0530

    updated method

--- src/app/services/food.service.ts
+++ src/app/services/food.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, sample } from 'rxjs';
 import { sample_foods, sample_tags } from 'src/data';
@@ -6,27 +6,33 @@ import { FOODS_BY_ID_URL, FOODS_BY_SEARCH_URL, FOODS_BY_TAG_URL, FOODS_TAGS_URL,
 import { Food } from '../shared/models/Food';
 import { Tag } from '../shared/models/tag';
 
+const headers= new HttpHeaders()
+  .set('content-type', 'application/json')
+  .set('Access-Control-Allow-Origin', '*')
+  .set('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+
 @Injectable({
   providedIn: 'root',
 })
+
 export class FoodService {
   constructor(private http: HttpClient) {}
   getAll(): Observable<Food[]> {
-    return this.http.get<Food[]>(FOODS_URL);
+    return this.http.get<Food[]>(FOODS_URL, { 'headers': headers });
   }
   getAllFoodsBySearchTerm(searchTerm: string) {
-    return this.http.get<Food[]>(FOODS_BY_SEARCH_URL + searchTerm)
+    return this.http.get<Food[]>(FOODS_BY_SEARCH_URL + searchTerm, { 'headers': headers })
   }
   getAllTags(): Observable<Tag[]>{
-    return this.http.get<Tag[]>(FOODS_TAGS_URL);
+    return this.http.get<Tag[]>(FOODS_TAGS_URL, { 'headers': headers });
   }
   getAllFoodsByTag(tag:string): Observable<Food[]>{
     return tag == "All"?
     this.getAll():
-    this.http.get<Food[]> (FOODS_BY_TAG_URL + tag);
+    this.http.get<Food[]> (FOODS_BY_TAG_URL + tag, { 'headers': headers });
   }
 
   getFoodById(foodId:string): Observable<Food>{
-    return this.http.get<Food>(FOODS_BY_ID_URL + foodId);
+    return this.http.get<Food>(FOODS_BY_ID_URL + foodId, { 'headers': headers });
   }
 }
